5db86b3879ceddc022a8b483cf959ad1b5de4931
Ashutosh Kumar <aashutosh.blaze@gmail.com>
2026-03-25 16:52:14 +0530

    Initial setup: Spring Boot + Supabase connection

---
+++ src/main/java/com/ashu/exectrack/ExectrackApplication.java
@@ -0,0 +1,13 @@
+package com.ashu.exectrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExectrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExectrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashu/exectrack/ExectrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.exectrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExectrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
